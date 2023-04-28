ac9d96a9f831e0c8c0fbdb6abb6953602c05b3a1
clintonO <cloat1@morgan.edu>
2023-04-28 12:14:05 -0400

    first batch of if else statements

--- bashproject.sh
+++ bashproject.sh
@@ -3,3 +3,15 @@ echo "This project is to showcase what i have learned."
 
 echo "Enter a car brand that you'd like to determine reliability for:"
 read brand
+
+if [[ "$brand" == "Toyota" ]]; then
+  reliability=9.0
+elif [[ "$brand" == "Honda" ]]; then
+  reliability=9.0
+elif [[ "$brand" == "Ford" ]]; then
+  reliability=7.5
+elif [[ "$brand" == "Chevrolet" ]]; then
+  reliability=7.0
+else
+  reliability=6.0
+fi
